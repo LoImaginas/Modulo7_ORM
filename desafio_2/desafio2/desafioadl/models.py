48d9a61fee90c860272cb651ea96b9d244f0a38f
Lolett <159793648+LoImaginas@users.noreply.github.com>
2024-07-02 22:32:27 -0400

    Update models.py

--- desafio_2/desafio2/desafioadl/models.py
+++ desafio_2/desafio2/desafioadl/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Tarea(models.Model):
     descripcion = models.CharField(max_length=255)
-    estado = models.BooleanField(default=False)
+    estado = models.BooleanField(default=False) # borrado logico
 
 class SubTarea(models.Model):
+    id = models.AutoField(primary_key=True)
     descripcion = models.CharField(max_length=255)
     estado = models.BooleanField(default=False)
-    tarea = models.ForeignKey(Tarea, related_name='subtareas', on_delete=models.CASCADE)
+    tarea = models.ForeignKey(Tarea, related_name='subtareas', on_delete=models.CASCADE) #Related name = para no agregar el SET
